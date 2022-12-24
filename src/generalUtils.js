8d0d6cde7b4339129213cf4eda1c65ea67c6aeed
Mansur <mensurowary@gmail.com>
2022-12-24 01:57:00 -0700

    Persisting the user

--- src/generalUtils.js
+++ src/generalUtils.js
@@ -3,7 +3,17 @@ function generateRandomNumber() {
 }
 
 export function generateRandomUser() {
-    return {
+    const existingUserStr = localStorage?.getItem("USER");
+    
+    if (existingUserStr) {
+        return JSON.parse(existingUserStr);
+    }
+
+    const newUser = {
         id: generateRandomNumber()
     }
+
+    localStorage?.setItem("USER", JSON.stringify(newUser));
+
+    return newUser;
 }
